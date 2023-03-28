965e55ee3f3cee308ba3f1238297a03b75f03b72
bsterwerf <bsterwerf@berkeley.edu>
2023-03-28 09:30:28 -0700

    Problems with the arg parser solved and the code now runs, making superficial changes from this point and going to add capability to process multiple background files

--- signal_background_merger.py
+++ signal_background_merger.py
@@ -9,12 +9,11 @@ import pyhepmc as hep
 import random as rd
 from datetime import datetime
 
-import helper
 import argparse
 
 if __name__ == '__main__':
     
-
+    
     parser = argparse.ArgumentParser(description='Merging Background and signal files')
     parser.add_argument('--one_background_particle', 
                         help='Set to True if all the background vertices in the file are to be connected temporally and spatially and False for backgrounds with many particles in the event like SR (synchrotron radiation)', 
@@ -37,12 +36,12 @@ if __name__ == '__main__':
     c_light  = 299.792458
 
     #Whether or not we move the signal event
-    shift=args[1]
+    shift=args.shifter
 
     #Depending on the background type.
     #SR Synchrotron radiation will be set too false since there will be many individual photons
     #
-    one_background_particle=args[0]
+    one_background_particle=args.one_background_particle
 
     #container for signal events
     sig_cont=[]
@@ -53,7 +52,7 @@ if __name__ == '__main__':
 
     #opens up the signal file and extracts all individual events stores these events in sig_cont
     #with hep.open("pythia8NCDIS_10x100_minQ2=1_beamEffects_xAngle=-0.025_hiDiv_1_000.hepmc") as f:
-    with hep.open(args[3]) as f:
+    with hep.open(args.Signal_File) as f:
         for event in f:
             #print(event)
             sig_cont.append(event)
@@ -61,7 +60,7 @@ if __name__ == '__main__':
 
     #opens up the signal file and extracts all individual events. stores these events in back_cont
     #with hep.open("photon_event16_03_2023_02_05_02.hepmc") as f:
-    with hep.open(args[4]) as f:
+    with hep.open(args.Background_File) as f:
         for event in f:
             #print(event)
             back_cont.append(event)
@@ -71,7 +70,7 @@ if __name__ == '__main__':
 
 
 
-    _____________________________________________________________________________________________________________________________________________
+    #_____________________________________________________________________________________________________________________________________________
 
 
 
@@ -81,7 +80,7 @@ if __name__ == '__main__':
         combo_cont.append(hep.GenEvent(hep.Units.GEV, hep.Units.MM))
         sig_particles=[]
         sig_vertices=[]
-        sig_time_shift=c_light*(args[2]*rd.random())
+        sig_time_shift=c_light*(args.Int_Window*rd.random())
         #sig_vertices.append(hep.GenVertex(sig_cont[i].event_pos()))
         
         #Stores the vertices of the event inside a vertex container. These vertices are in increasing order so we can index them with [abs(vertex_id)-1]
@@ -92,7 +91,7 @@ if __name__ == '__main__':
                 position=position+hep.FourVector(x=0,y=0,z=0,t=sig_time_shift)
             v1=hep.GenVertex(position)
             sig_vertices.append(v1)
-        print(len(sig_vertices))
+        #print(len(sig_vertices))
         
         #copies the particles and attaches them to their corresponding vertices
         for particle in sig_cont[i].particles:
@@ -126,7 +125,7 @@ if __name__ == '__main__':
         
         #Standard use except for SR backgrounds. Will only shift the background event when this conditional is triggered.
         if one_background_particle:
-            back_time_shift=c_light*(args[2]*rd.random())
+            back_time_shift=c_light*(args.Int_Window*rd.random())
 
 
         for vertex in back_cont[i].vertices:
@@ -134,7 +133,7 @@ if __name__ == '__main__':
             position=vertex.position
             #When the background event has many particles separated in time, like SR backgrounds, trigger this coniditional by changing the one_background_particle to False
             if one_background_particle==False:
-                back_time_shift=c_light*(args[2]*rd.random())
+                back_time_shift=c_light*(args.Int_Window*rd.random())
             position=position+hep.FourVector(x=0,y=0,z=0,t=back_time_shift)
             v1=hep.GenVertex(position)
             back_vertices.append(v1)
@@ -175,11 +174,11 @@ if __name__ == '__main__':
     # datetime object containing current date and time
     now = datetime.now()
     
-    print("now =", now)
+    #print("now =", now)
 
     # dd/mm/YY H:M:S
     dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
-    print("date and time =", dt_string)	
+    #print("date and time =", dt_string)	
     #name='combo_event{}.hepmc'.format(dt_string)
 
     name='Dis_1000_event{}.hepmc'.format(dt_string)
@@ -188,3 +187,4 @@ if __name__ == '__main__':
     with hep.WriterAsciiHepMC2(name) as f:
         for i in combo_cont:
             f.write_event(i)
+    f.close()
