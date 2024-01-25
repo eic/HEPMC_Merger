6c3a3bdde52a4f4e2726ea1639f7aa5d9e4072fb
Kolja Kauder <kkauder@gmail.com>
2024-01-25 17:00:49 -0500

    Only the business logic missing

--- SignalBackgroundMerger.cpp
+++ SignalBackgroundMerger.cpp
@@ -8,8 +8,16 @@
 #include <algorithm>
 #include <numeric>
 #include <stdexcept>
+#include <chrono>
+#include <cmath>
+#include <sys/resource.h>
 
 #include <HepMC3/ReaderFactory.h>
+#include <HepMC3/WriterAscii.h>
+#include "HepMC3/WriterRootTree.h"
+#include "HepMC3/GenRunInfo.h"
+#include "HepMC3/GenEvent.h"
+#include "HepMC3/Print.h"
 
 #include "argparse/argparse.hpp"
 
@@ -33,11 +41,73 @@ class SignalBackgroundMerger {
 public:
 
   SignalBackgroundMerger(int argc, char* argv[]) {
+    auto t0 = std::chrono::high_resolution_clock::now();
+
     // Parse arguments, print banner, open files, initialize rng
     digestArgs(argc, argv);
     rng.seed( rngSeed );
     banner();
+    if (outputFile != "" ) {
+      outputFileName = outputFile;
+    } else {
+      outputFileName = nameGen();
+    }
+    cout << "Writing to " << outputFileName << endl;
+
+    makeDicts ( signalFile, signalFreq, true );
+    makeDicts ( bg1File, bg1Freq );
+    makeDicts ( bg2File, bg2Freq );
+    makeDicts ( bg3File, bg3Freq );
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::cout << "Initiation time: " << std::round(std::chrono::duration<double, std::chrono::minutes::period>(t1 - t0).count()) << " min" << std::endl;
+    std::cout << "\n==================================================================\n" << std::endl;
+
   }
+
+  void merge(){
+    auto t1 = std::chrono::high_resolution_clock::now();
+
+    // Open output file    
+    std::shared_ptr<HepMC3::Writer> f;
+    if (rootFormat)
+      f = std::make_shared<HepMC3::WriterRootTree>(outputFileName);
+    else
+      f = std::make_shared<HepMC3::WriterAscii>(outputFileName);
+    
+    // Slice loop
+    int i = 0;
+    for (i = 0; i<nSlices; ++i ) {
+      if (i % 100 == 0 || verbose ) squawk(i);
+      auto hepSlice = mergeSlice(i);
+      if (!hepSlice) {
+	std::cout << "Exhausted signal source." << std::endl;
+	break;
+      }
+      hepSlice->set_event_number(i);
+      f->write_event(*hepSlice);
+      i++;
+    }
+    std::cout << "Finished all requested slices." << std::endl;
+
+    int slicesDone = i;
+    auto t2 = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Slice loop time: " << std::round(std::chrono::duration<double, std::chrono::minutes::period>(t2 - t1).count()) << " min" << std::endl;
+    std::cout << " -- " << std::round(std::chrono::duration<double>(t2 - t1).count() / i) << " sec / slice" << std::endl;
+
+    // clean up, close all files
+    auto it = sigDict.find(signalFile);
+    if (it != sigDict.end()) {
+      it->second.first->close();
+    }
+    for (auto& it : freqDict) {
+      it.second.first->close();
+    }
+    f->close();
+	
+  }
+  
   // ---------------------------------------------------------------------------
   void digestArgs(int argc, char* argv[]) {
     // Handle the command line tedium
@@ -82,7 +152,12 @@ public:
     args.add_argument("-o", "--outputFile")
       .default_value(std::string(""))
       .help("Specify the output file name. By default it will be auto-generated.");
-    
+
+    args.add_argument("-r", "--rootFormat")
+      .default_value(false)
+      .implicit_value(true)
+      .help("Use hepmc.root output format, default is false.");
+	
     args.add_argument("-w", "--intWindow")
       .default_value(2000.0)
       .help("Length of the integration window in nanoseconds. Default is 2000.");
@@ -125,10 +200,14 @@ public:
     bg3Freq = args.get<double>("--bg3Freq");
     
     outputFile = args.get<std::string>("--outputFile");
+    rootFormat = args.get<bool>("--rootFormat");
     intWindow  = args.get<double>("--intWindow");
     nSlices    = args.get<int>("--nSlices");
     squashTime = args.get<bool>("--squashTime");
-    rngSeed    = args.get<int>("--rngSeed"); 
+    rngSeed    = args.get<int>("--rngSeed");
+    verbose    = args.get<bool>("--verbose");
+
+      
   }
   
   // ---------------------------------------------------------------------------
@@ -184,51 +263,127 @@ public:
     if (freq <= 0) {
       std::cout << "Reading in all events from " << fileName << std::endl;
       std::vector<HepMC3::GenEvent> events;
-      std::vector<double> weights;
-      double weight;
-      HepMC3::GenEvent event;
 
-      HepMC3::GenEvent evt(HepMC3::Units::GEV,HepMC3::Units::MM);
-      adapter->read_event(evt);
+      std::vector<weightedEvent> weightedEvents;
+	// HepMC3::GenEvent event;
 
-	
-      // while (File >> event >> weight) {
-      // 	if (weight > 0) {
-      // 	  events.push_back(event);
-      // 	  weights.push_back(weight);
-      // 	}
-      // }
-      // File.close();
 
-      std::sort(weights.begin(), weights.end());
-      double avgRate = std::accumulate(weights.begin(), weights.end(), 0.0) / weights.size();
-      avgRate *= 1e-9;
-      std::cout << "Average rate is " << avgRate << " GHz" << std::endl;
-      for (auto& weight : weights) {
-	weight /= avgRate;
+      while(!adapter->failed()) {
+	HepMC3::GenEvent evt(HepMC3::Units::GEV,HepMC3::Units::MM);
+	adapter->read_event(evt);
+	// HepMC3::Print::listing(evt);
+	// HepMC3::Print::content(evt);
+
+	// remove events with 0 weight - note that this does change avgRate = <weight> (by a little)
+	if (evt.weight() > 0){
+	  weightedEvents.push_back({evt,evt.weight()});
+	}
       }
-      weightDict[fileName] = {events, weights};
+      adapter->close();
+      
+      double avgRate = 0.0;
+      for ( auto w : weightedEvents ){ avgRate += w.second;}
+      avgRate /= weightedEvents.size();
+      avgRate *= 1e-9; // convert to 1/ns == GHz
+      std::cout << "Average rate is " << avgRate << " GHz" << std::endl;
+      for ( auto w : weightedEvents ) {
+	w.second /= avgRate;
+      }
+
+      // Optional to sort, may speed up random drawing
+      std::sort(weightedEvents.begin(), weightedEvents.end(), [](auto &left, auto &right) {
+	  return left.second < right.second;
+	});
+      
+      weightDict[fileName] = {weightedEvents, avgRate};
+
       return;
     }
 
     // Not signal and not weighted --> update freqDict
     freqDict[fileName] = {adapter, freq};
   }
+
+
+   // ---------------------------------------------------------------------------
+  bool hasEnding (std::string const &fullString, std::string const &ending) {
+    if (fullString.length() >= ending.length()) {
+      return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
+    } else {
+      return false;
+    }
+  }
+  
+  // ---------------------------------------------------------------------------
+  std::string nameGen() {
+    std::string name = signalFile;
+    if (nSlices > 0) {
+        size_t pos = name.find(".hepmc");
+        if (pos != std::string::npos) {
+	  name.replace(pos, 6, "_n_" + std::to_string(nSlices) + ".hepmc");
+        }
+    }
+    if ( rootFormat && !hasEnding(name,".root")){
+      name.append(".root");
+    }
+    name = "bgmerged_" + name;
+    return name;
+}
+
+  // ---------------------------------------------------------------------------
+  void squawk(int i) {
+    struct rusage r_usage;
+    getrusage(RUSAGE_SELF, &r_usage);
+
+    std::cout << "Working on slice " << i + 1 << std::endl;
+    std::cout << "Resident Memory " << r_usage.ru_maxrss / 1024.0 << " MB" << std::endl;
+  }
   // ---------------------------------------------------------------------------
 
+  std::unique_ptr<HepMC3::GenEvent> mergeSlice(int i) {
+    auto hepSlice = std::make_unique<HepMC3::GenEvent>(HepMC3::Units::GEV, HepMC3::Units::MM);
+    
+    // try {
+    //   hepSlice = addFreqEvents(args["signalFile"], hepSlice, true);
+    // } catch (std::ifstream::failure& e) {
+    //   std::cerr << "Error: " << e.what() << '\n';
+    //   return HepMC3::GenEvent();
+    // }
+    
+    // for (const auto& fileName : freqDict) {
+    //   hepSlice = addFreqEvents(fileName.first, hepSlice);
+    // }
+    
+    // for (const auto& fileName : weightDict) {
+    //   hepSlice = addWeightedEvents(fileName.first, hepSlice);
+    // }
+
+    return hepSlice;
+  };
+
+    // ---------------------------------------------------------------------------
+
+    // ---------------------------------------------------------------------------
+
+  
 private:
   std::default_random_engine rng;
   string signalFile, bg1File, bg2File, bg3File;
   double signalFreq, bg1Freq, bg2Freq, bg3Freq;
   string outputFile;
+  string outputFileName;
+  bool rootFormat;
   double intWindow;
   int nSlices; // should be long, but argparse cannot read that
   bool squashTime;
   int rngSeed;  // should be unsigned, but argparse cannot read that
-
+  bool verbose;
+  
   std::map<std::string, std::pair< std::shared_ptr<HepMC3::Reader>,double> > sigDict;
-  std::map<std::string, std::pair<std::vector<HepMC3::GenEvent>, std::vector<double>>> weightDict;
-  std::map<std::string, std::pair<std::shared_ptr<HepMC3::Reader>,double> > freqDict;
+  std::map<std::string, std::pair< std::shared_ptr<HepMC3::Reader>,double> > freqDict;
+
+  typedef std::pair<HepMC3::GenEvent,double> weightedEvent;
+  std::map<std::string, std::pair<std::vector<weightedEvent>,double>> weightDict;
 
 };
 
@@ -236,10 +391,13 @@ private:
 int main(int argc, char* argv[]) {
   cout << argc << "  " << argv[0] << endl;
 
+  auto t0 = std::chrono::high_resolution_clock::now();
   // Create an instance of SignalBackgroundMerger
-  SignalBackgroundMerger merger (argc, argv);
+  SignalBackgroundMerger sbm (argc, argv);
+  // sbm.merge();
 
-
+  std::cout << "\n==================================================================\n";
+  std::cout << "Overall running time: " << std::round(std::chrono::duration<double, std::chrono::minutes::period>(std::chrono::high_resolution_clock::now() - t0).count()) << " min" << std::endl;
   
   
   return 0;
