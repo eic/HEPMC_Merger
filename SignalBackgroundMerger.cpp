ff6b0379cbe7a385ea3328f4b5514dad3dbe8c8d
EIC Container Build Service <gitlab@eicweb.phy.anl.gov>
2025-09-26 12:30:43 -0400

    Support weights for single event signal sources.

--- SignalBackgroundMerger.cpp
+++ SignalBackgroundMerger.cpp
@@ -241,7 +241,12 @@ public:
       .default_value(0.0)
       .scan<'g', double>()
       .help("Signal frequency in kHz. Default is 0 to have exactly one signal event per slice. Set to the estimated DIS rate to randomize.");
-    
+
+    args.add_argument("-sw", "--signalWeight")
+      .default_value(false)
+      .implicit_value(true)
+      .help("Preserve signal weight from the HepMC header. This makes sense only if signalFreq is set to 0 (and even then it may not, if background is present). Default is false.");
+
     args.add_argument("-S", "--signalSkip")
       .default_value(0)
     .scan<'i', int>()
@@ -302,6 +307,11 @@ public:
     // Access arguments using args.get method
     signalFile = args.get<std::string>("--signalFile");
     signalFreq = args.get<double>("--signalFreq");
+    signalWeight = args.get<bool>("--signalWeight");
+    if (signalWeight && signalFreq != 0.0) {
+      std::cerr << "Error: --signalWeight can only be used if --signalFreq is set to 0." << std::endl;
+      exit(1);
+    }
     signalSkip = args.get<int>("--signalSkip");
     signalStatus = args.get<int>("--signalStatus");
     backgroundFiles = parse_backgrounds(args.get<std::vector<std::string>>("--bgFile"));
@@ -313,6 +323,7 @@ public:
     rngSeed    = args.get<int>("--rngSeed");
     verbose    = args.get<bool>("--verbose");
 
+    
   }
   
   // ---------------------------------------------------------------------------
@@ -567,6 +578,10 @@ public:
 
       HepMC3::GenEvent inevt;
       adapter->read_event(inevt);
+      if (signal && signalWeight){
+        hepSlice->weights() = inevt.weights();
+      }
+
 
       if (squashTime) time = 0;
       particleCount += insertHepmcEvent( inevt, hepSlice, time, baseStatus, signal);
@@ -705,6 +720,7 @@ public:
   double signalFreq;
   int signalSkip;
   int signalStatus;
+  bool signalWeight;
   std::vector<BackgroundConfig> backgroundFiles;  
   string outputFile;
   string outputFileName;
